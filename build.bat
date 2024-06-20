3a3669de4d70e00a5e4de2eb78b7a25c733aaa03
israelmanzi <manziisrael99@gmail.com>
2024-06-20 14:08:15 +0300

    fx

--- build.bat
+++ build.bat
@@ -1,11 +1,6 @@
 @echo off
 
-git pull origin main -q
-if %errorlevel% ne 0 (
-    exit /b 1
-)
-
-g++ -o main.exe main.cpp
+git pull origin main -q && g++ -o main.exe main.cpp
 if errorlevel 1 (
     echo Compilation failed.
     exit /b 1
